f03af01cf2bd02b83dc538950536dab46ae4bb33
M Saim <msaimkzmsk2008@gmail.com>
2024-02-14 12:51:01 +0500

    PHP Operator

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+//Arithematic Operator
+
+print 'Arithematic Operator'.'<br>';
+$num1=8;
+$num2=9;
+
+echo $num1+$num2 .'<br>';
+echo $num1-$num2 .'<br>';
+echo $num1*$num2 .'<br>';
+echo $num1/$num2 .'<br>';
+
+
+//Relational Operator
+
+print 'Relational Operator'.'<br>';
+
+$num3=9;
+$num4=8;
+
+echo $num3==$num4;
+echo '<br>';
+
+echo $num3!=$num4;
+echo '<br>';
+
+echo $num3===$num4;
+echo '<br>';
+
+echo $num3<$num4;
+echo '<br>';
+
+echo $num3>$num4;
+echo '<br>';
+
+
+//Logical Opeartor
+
+print 'Logical Operator'.'<br>';
+
+$name='Usama';
+$email='Ali123';
+
+print $name=='Ali' || $name=='Usama' && $email=='Ali123';
+
+?>
+
