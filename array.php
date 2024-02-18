048b26ea5f3588e6d7c2cdb30ac4eeecd1bdc075
M Saim <msaimkzmsk2008@gmail.com>
2024-02-18 11:24:23 +0500

    Loop & Array in PHP

---
+++ array.php
@@ -0,0 +1,33 @@
+<?php
+
+//Arry in PHP
+
+//Numeric Array
+
+// first Method
+
+$color=['red','green','yellow','purple'];
+
+foreach($color as $colors){
+    echo $colors .'<br>';
+}
+
+//Seocnd Method
+
+$color = ("red","green");
+foreach($color as $colors){
+    echo $colors .'<br>';
+}
+
+// Third Method 
+
+$car[0]='mercedes';
+$car[1]='bugatti';
+$car[2]='suzuki';
+$car[3]='toyotu';
+
+foreach($car as $cars){
+    echo $cars .'<br>';
+
+}
+?>
