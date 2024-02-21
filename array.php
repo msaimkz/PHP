fa4f129927f68c6cc66b6ca38d239888928c7eac
M Saim <msaimkzmsk2008@gmail.com>
2024-02-21 12:35:38 +0500

    Array & Loop in PHP

--- array.php
+++ array.php
@@ -2,32 +2,68 @@
 
 //Arry in PHP
 
-//Numeric Array
+//Associative Array
 
 // first Method
 
-$color=['red','green','yellow','purple'];
+$Student=Array(
+    'Ali'=>20,
+    'Yahya'=>21,
+    'Zubir'=>22,
+    'Ayan'=>10,
+);
 
-foreach($color as $colors){
-    echo $colors .'<br>';
+//Assending Order in value
+asort($Student);
+
+// Assending Order in Key
+
+ ksort($Student);
+
+//  Descending order in value
+
+ arsort($Student);
+
+//  Descending order in Key
+
+ krsort($Student);
+
+// Count Array Length
+
+ echo count($Student);
+
+
+foreach($Student as $keyd => $obj){
+    echo  $keyd.'     '.'age is'.'     '. $obj.'<br>';
 }
 
-//Seocnd Method
 
-$color = ("red","green");
-foreach($color as $colors){
-    echo $colors .'<br>';
+// Second Method
+
+$Student['ali']=20;
+$Student['usama']=30;
+$Student['khizer']=10;
+
+echo $Student['khizer'];
+
+
+// MultiDimensional Array
+
+$number=[
+    [1,2,4,5],
+    [9,8,0,4],
+    [7,24,34,32],
+];
+
+echo $number[2][2];
+
+// Nested Loop
+
+for($row=0;$row<3;$row++){
+    for($col=0;$col<4;$col++){
+        echo $number[$row][$col].'   ';
+    }
+    echo '<br>';
 }
 
-// Third Method 
-
-$car[0]='mercedes';
-$car[1]='bugatti';
-$car[2]='suzuki';
-$car[3]='toyotu';
-
-foreach($car as $cars){
-    echo $cars .'<br>';
-
-}
 ?>
