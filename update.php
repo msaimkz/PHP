2e5d59749f66a198c9579d7acfd2b86dec8fc04d
M Saim <msaimkzmsk2008@gmail.com>
2024-03-08 11:18:07 +0500

    Image Updating

--- update.php
+++ update.php
@@ -26,7 +26,12 @@ else{
     $temname= $_FILES['image']['tmp_name'];
     $upload= "upload/" .$img;
     move_uploaded_file($temname,$upload);
+    if($temname != ""){
     $query= "update `Student` set `name`='$name', `email`='$email', `city`='$city' , `image`='$img' where id='$id' ";
+    }
+    else{
+    $query= "update `Student` set `name`='$name', `email`='$email', `city`='$city'  where id='$id' ";
+    }
     $result=mysqli_query($con,$query);
 
     if($result){
@@ -75,6 +80,8 @@ else{
       <label for="exampleInputEmail1" class="form-label mt-4"> City</label>
       <input type="text"  name="city" class="form-control"  value="<?php echo $row['city']?>" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter City">
     </div>
+
+    <img src="upload/<?php echo $row["image"]?>" alt="" height="100px"> <br><br>
     <div class="form-group">
       <label for="exampleInputEmail1" class="form-label mt-4"> Image</label>
       <input type="file"  name="image" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
